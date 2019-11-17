984fd688b5ece9237d06d5c549361a5c7b957a1b
Chet Aldrich <chetaldrich@gmail.com>
2019-11-17 17:27:05 -0500

    add test case to make sure switching decoder works

--- api_client_test.go
+++ api_client_test.go
@@ -1,7 +1,9 @@
 package goodreads
 
 import (
+	"encoding/json"
 	"encoding/xml"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
@@ -11,20 +13,33 @@ import (
 )
 
 func TestHttpClient_Get(t *testing.T) {
-	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, "/foo/bar?p1=v1&p2=v2", r.URL.String())
-		w.Write([]byte(`<?xml version="1.0" encoding="UTF-8"?><response><id>SampleID</id></response>`))
-	}))
-	defer s.Close()
-
-	v := url.Values{}
-	v.Set("p1", "v1")
-	v.Set("p2", "v2")
-	var res struct {
-		ID string `xml:"id"`
+	testCases := []struct {
+		DecoderType string
+		Decoder     func([]byte, interface{}) error
+		Data        []byte
+	}{
+		{"xml", xml.Unmarshal, []byte(`<?xml version="1.0" encoding="UTF-8"?><response><id>SampleID</id></response>`)},
+		{"json", json.Unmarshal, []byte(`{ "id": "SampleID" }`)},
 	}
-	h := HTTPClient{Client: http.DefaultClient, ApiRoot: s.URL, Verbose: true}
-	err := h.Get("foo/bar", xml.Unmarshal, v, &res)
-	assert.Nil(t, err)
-	assert.Equal(t, "SampleID", res.ID)
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("with %s decoder", tc.DecoderType), func(t *testing.T) {
+			s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				assert.Equal(t, "/foo/bar?p1=v1&p2=v2", r.URL.String())
+				_, _ = w.Write(tc.Data)
+			}))
+			defer s.Close()
+
+			v := url.Values{}
+			v.Set("p1", "v1")
+			v.Set("p2", "v2")
+			var res struct {
+				ID string `xml:"id"`
+			}
+			h := HTTPClient{Client: http.DefaultClient, ApiRoot: s.URL, Verbose: true}
+			err := h.Get("foo/bar", tc.Decoder, v, &res)
+			assert.Nil(t, err)
+			assert.Equal(t, "SampleID", res.ID)
+		})
+	}
 }
